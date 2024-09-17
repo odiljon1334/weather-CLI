eb99db9f0adeb5f076a01c5bd37d34fa2a5f49e6
odiljon1334 <odil1334@gmail.com>
2024-09-17 17:39:57 +0500

    feat: create standards errors

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ const saveToken = async token => {
     }
 }
 
+const getForcast = async () => {
+    try {
+        const response = await getWeather(process.env.CITY ?? 'Uzbekistan');
+        console.log(response);
+    } catch (error) {
+        if (error?.response?.status == 400) {
+            printError('City not found');
+        } else if (error?.response?.status == 401) {
+            printError('Invalid token');
+        } else {
+            printError(error.message);
+        }
+    }
+}
 const startCLI = () => {
     const args = getArgs(process.argv);
     if (args.h) {
@@ -29,6 +43,7 @@ const startCLI = () => {
         return saveToken(args.t);
         // save token
     }
-    getWeather(process.env.CITY ?? 'Uzbekistan');
+    getForcast()
+
 }
 startCLI();
